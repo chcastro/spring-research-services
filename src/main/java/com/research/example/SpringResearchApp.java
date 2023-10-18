2650012dbd72a481086453be2b99bc51ec22ca6b
F. Christian Castro Peralta <f.christian castro peralta@>
2023-10-17 23:15:01 -0500

    Initial commit

---
+++ src/main/java/com/research/example/SpringResearchApp.java
@@ -0,0 +1,13 @@
+package com.research.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResearchApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResearchApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/research/example/SpringResearchAppTests.java
@@ -0,0 +1,13 @@
+package com.research.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResearchAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
